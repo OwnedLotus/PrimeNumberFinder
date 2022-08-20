48fd72e1be4cde98ff4f3be7662531f19bc2b3e7
Jonah Ringdahl <46978061+OwnedLotus@users.noreply.github.com>
2022-08-20 18:13:23 -0400

    Created Loop and fixed internal timing report

--- Program.cs
+++ Program.cs
@@ -1,31 +1,35 @@
-﻿Console.WriteLine("Input a number:");
+﻿Console.WriteLine("Press 'q' to Quit");
+Console.WriteLine("Input a number:");
 string? input = Console.ReadLine();
 int value = 0;
 
-
-bool s = int.TryParse(input, out value);
-
-if (s)
+while (input != "q")
 {
-    var startTime = DateTime.Now;
+    bool s = int.TryParse(input, out value);
 
-    var dictionaryInts = GeneratePrimeValues(value);
-
-    if (dictionaryInts.Count == 0)
-        Console.WriteLine("This number is Prime!");
-    
-    foreach (var pair in dictionaryInts)
+    if (s)
     {
-        Console.WriteLine($"{pair.Key}, {pair.Value}");
+        var startTime = DateTime.Now;
+
+        var dictionaryInts = GeneratePrimeValues(value);
+
+        var timeElapsed = DateTime.Now - startTime;
+        
+        if (dictionaryInts.Count == 0)
+            Console.WriteLine("This number is Prime!");
+    
+        foreach (var pair in dictionaryInts)
+        {
+            Console.WriteLine($"{pair.Key}, {pair.Value}");
+        }
+
+        Console.WriteLine("Time Elapsed {0}", timeElapsed);
     }
-
-    var timeElapsed = DateTime.Now - startTime;
-
-    Console.WriteLine("Time Elapsed {0}", timeElapsed);
-}
-else
-{
-    Console.WriteLine("Not an Integer");
+    else
+    {
+        Console.WriteLine("Not an Integer");
+    }
+    input = Console.ReadLine();
 }
 
 
@@ -34,9 +38,9 @@ Dictionary<int,int> GeneratePrimeValues(int value)
     var factorDictionary = new Dictionary<int, int>();
 
     if (value < 0)
-    {
         throw new ArgumentOutOfRangeException(nameof(value));
-    }
+    else if (value == 2)
+        return new Dictionary<int, int>();
 
     for (int i = 2; i < Math.Sqrt(value) + 1; i++)
     {
